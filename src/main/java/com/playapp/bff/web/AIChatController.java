3fe395a934b4ea6d2c1b8ab52f8d471610002bfd
miria <miria@miriamcbl>
2024-03-26 12:18:16 +0100

    adding OpenAI API integration + changing apiKey

---
+++ src/main/java/com/playapp/bff/web/AIChatController.java
@@ -0,0 +1,35 @@
+package com.playapp.bff.web;
+
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+public class AIChatController {
+    private final OpenAiChatClient chatClient;
+
+    @Autowired
+	public AIChatController(OpenAiChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/ai/generate")
+	public String generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+		return chatClient.call(message);
+    }
+
+	@GetMapping("/ai/generateStream")
+	public Flux<ChatResponse> generateStream(
+			@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+		Prompt prompt = new Prompt(new UserMessage(message));
+		Flux<ChatResponse> cont = chatClient.stream(prompt);
+		return cont;
+	}
+}
