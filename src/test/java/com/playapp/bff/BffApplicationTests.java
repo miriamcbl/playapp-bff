5b6cd0ce89c5a8ff1af88b424c8cb80d471dbe07
miria <miria@miriamcbl>
2024-03-26 11:56:37 +0100

    adding Maven project architecture

---
+++ src/test/java/com/playapp/bff/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playapp.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
