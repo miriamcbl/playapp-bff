7bcab2cadb11eae443a573215b1b9490c2342e1b
miriamcbl <miriamcorbla@gmail.com>
2024-04-20 12:10:25 +0200

    changing jupiter junit + deleting dependency junit vintage

--- src/test/java/com/playapp/bff/service/supplier/bean/MeasurementTest.java
+++ src/test/java/com/playapp/bff/service/supplier/bean/MeasurementTest.java
@@ -1,9 +1,8 @@
 package com.playapp.bff.service.supplier.bean;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class MeasurementTest {
 
